339f48408a420127bd2bafc137893f22c3c0972b
Víctor M. Rivas <vrivas@ujaen.es>
2020-09-16 18:25:57 +0200

    Initial commit

---
+++ app/src/main/java/es/vrivas/apartado4_ejemploguiado_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.vrivas.apartado4_ejemploguiado_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
