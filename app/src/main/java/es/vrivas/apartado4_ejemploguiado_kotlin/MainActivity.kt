a8c5cf919ba9bb84cf5512005a732916dca09866
Víctor M. Rivas <vrivas@ujaen.es>
2020-09-16 21:06:49 +0200

    Click del botón cambiar imagen

--- app/src/main/java/es/vrivas/apartado4_ejemploguiado_kotlin/MainActivity.kt
+++ app/src/main/java/es/vrivas/apartado4_ejemploguiado_kotlin/MainActivity.kt
@@ -1,11 +1,20 @@
 package es.vrivas.apartado4_ejemploguiado_kotlin
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun cambiar_imagen_click(v: View) {
+        imagen.setImageResource(R.drawable.isla_kotlin)
+        tv_titulo_imagen.setText(R.string.isla_kotlin)
     }
 }
