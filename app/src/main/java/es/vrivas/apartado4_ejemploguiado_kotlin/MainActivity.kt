7058d52516ac5673bac608f7831fc7bf7b953d78
Víctor M. Rivas <vrivas@ujaen.es>
2020-09-26 16:52:28 +0200

    Guardamos el InstanceState para saber qué imagen se estaba mostrando cuando giramos la pantalla.

--- app/src/main/java/es/vrivas/apartado4_ejemploguiado_kotlin/MainActivity.kt
+++ app/src/main/java/es/vrivas/apartado4_ejemploguiado_kotlin/MainActivity.kt
@@ -9,13 +9,47 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var seleccion_isla = false;
+    // Propiedad que controla si estamos viendo la foto de la isla o no (que sería la del logo de la UJA)
+    var seleccion_isla: Boolean = false;
+
+    // Algunas "constantes"
+    val KEY_SELECCION_ISLA: String = "SELECCION_ISLA"
+
+    /**
+     * Método inicial cuando se crea la actividad
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
 
+    /**
+     * Almacenamos el valor de la propiedad seleccion_isla
+     * @param outState Bundle en el que se almacenan los valores que luego queremos recuperar
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean( KEY_SELECCION_ISLA, seleccion_isla)
+        super.onSaveInstanceState(outState)
+    }
+
+    /**
+     * Recuperamos el valor de la propiedad seleccion_isla
+     * @param savedInstanceState Bundle en el que se habían almacenado los valores que luego queremos recuperar
+     */
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        seleccion_isla=savedInstanceState.getBoolean(KEY_SELECCION_ISLA)
+        actualizarImagenTitulo()
+    }
+
+    /**
+     * Actualiza la imagen y el título que aparecen al usuario
+     */
+    private fun actualizarImagenTitulo() {
+        imagen.setImageResource(if (seleccion_isla) R.drawable.isla_kotlin else R.drawable.iconoheader)
+        tv_titulo_imagen.setText(if (seleccion_isla) R.string.isla_kotlin else R.string.logo_uja)
+    }
     /**
      * Callback de evento click del botón "CAMBIAR IMAGEN".
      * Intercambia las imágenes entre si.
@@ -24,8 +58,7 @@ class MainActivity : AppCompatActivity() {
     @Suppress("UNUSED_PARAMETER")
     fun cambiar_imagen_click(v: View) {
         seleccion_isla=!seleccion_isla
-        imagen.setImageResource(if (seleccion_isla) R.drawable.isla_kotlin else R.drawable.iconoheader)
-        tv_titulo_imagen.setText(if (seleccion_isla) R.string.isla_kotlin else R.string.logo_uja)
+        actualizarImagenTitulo()
     }
 
     /**
